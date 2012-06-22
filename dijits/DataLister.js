e4b1aad2ef4f946f266c60bcdaba1853a6f3e227
Andrew Wheelwright <wheelwright.tech@gmail.com>
2012-06-21 18:22:46 -0600

    Fixed up data lister to handle activation

--- dijits/DataLister.js
+++ dijits/DataLister.js
@@ -15,6 +15,7 @@ dojo.declare('czarTheory.dijits.DataLister',[dijit._Widget, dijit._Templated],{
 	draggable: false,
 	objectStore: null,
 	itemConstructor: null,
+	dataItems: null,
 	idProperty: 'id',
 	templateString: dojo.cache('czarTheory.dijits', 'DataLister.html'),
 	_activeItem: null,
@@ -26,6 +27,7 @@ dojo.declare('czarTheory.dijits.DataLister',[dijit._Widget, dijit._Templated],{
 			this.objectStore = new czarTheory.store.JsonRest({target: this.target});
 		}
 
+		this.dataItems = {};
 		this.inherited(arguments);
 	},
 
@@ -36,9 +38,7 @@ dojo.declare('czarTheory.dijits.DataLister',[dijit._Widget, dijit._Templated],{
 		var _this = this;
 		dojo.when(this.objectStore.query(),function(results){
 			for(var i=0;i<results.length;i++){
-				var data = results[i];
-				var item = _this.itemConstructor({properties:data, animateOnCreate:false, idProperty:_this.idProperty});
-				item.placeAt(_this.storeContentsNode);
+				_this._addRecord.call(_this, results[i]);
 			}
 		},function(error){
 			console.log('error retreiving results back from server: ',error);
@@ -58,16 +58,12 @@ dojo.declare('czarTheory.dijits.DataLister',[dijit._Widget, dijit._Templated],{
 	reload: function(callback){
 		var _this = this;
 
-		var oldies = dijit.findWidgets(_this.storeContentsNode);
-		console.log('oldies:',oldies);
-		dojo.forEach(oldies, function(w){w.destroyRecursive();});
-
+		dojo.forEach(this.dataItems, function(w){w.destroyRecursive();});
+		this.dataItems = {};
 		dojo.when(this.objectStore.query(),function(results){
 			_this.numItems = results.length;
 			for(var i=0;i<results.length;i++){
-				var data = results[i];
-				var item = _this.itemConstructor({properties:data, animateOnCreate:false, idProperty:_this.idProperty});
-				item.placeAt(_this.storeContentsNode);
+				_this._addRecord.call(_this, results[i]);
 			}
 
 			callback(results.length);
@@ -75,6 +71,17 @@ dojo.declare('czarTheory.dijits.DataLister',[dijit._Widget, dijit._Templated],{
 			console.log('error retreiving results back from server: ',error);
 		});
 
+	},
+
+	_addRecord: function (data) {
+		if (this.dataItems.hasOwnProperty(data.id)) {
+			console.warn('You\'re overwritting id #' + data.id);
+			this.dataItems[data.id].destroyRecursive();
+		}
+
+		var item = this.itemConstructor({properties:data, animateOnCreate:false, idProperty:this.idProperty});
+		this.dataItems[data.id] = item;
+		item.placeAt(this.storeContentsNode);
 	},
 
 	_activateItem: function(widget){
