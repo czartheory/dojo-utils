79be13522352039fd6aff2272b7853b68ae15902
Matthew Larson <matthew@czarTheory.com>
2012-07-21 00:12:22 -0600

    this is for inlining the form

---
+++ dijits/_InlineCrudLister.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2011 Czar Theory, LLC
+ * All rights reserved.
+ */
+
+dojo.provide("czarTheory.dijits._InlineCrudLister");
+
+dojo.require("czarTheory.dijits._CrudLister");
+
+dojo.declare("czarTheory.dijits._InlineCrudLister",[czarTheory.dijits._CrudLister],{
+
+	,startup:function(){
+		this.inherited(arguments);
+
+		dojo.connect(this.deleteAnchor, "onclick", this, function(evt){
+			dojo.stopEvent(evt);
+			this._currentItem = this._activeItem;
+			if(this._confirmDeleteDialog) {
+                this._confirmDeleteDialog.show();
+            } else {
+                this._deleteCurrent();
+            }
+		});
+	}
+
+	,_activateItem: function(widget){
+		this.inherited(arguments);
+        this._currentItem = this._activeItem;
+        this._prepFormForUpdate();
+	}
+});
