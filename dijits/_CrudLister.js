7e1a6c3baf6fde512042e30b25f6b3cb2ea4dff1
Andrew Wheelwright <wheelwright.tech@gmail.com>
2012-08-07 19:29:10 -0600

    Syntax now JSLint compliant; fixed failure to activate item on delete/update.

--- dijits/_CrudLister.js
+++ dijits/_CrudLister.js
@@ -7,282 +7,302 @@ dojo.provide("czarTheory.dijits._CrudLister");
 dojo.require('czarTheory.dijits.DataLister');
 dojo.require('dijit.Dialog');
 
-dojo.declare("czarTheory.dijits._CrudLister",[
-	czarTheory.dijits.DataLister,
-], {
+dojo.declare("czarTheory.dijits._CrudLister", [czarTheory.dijits.DataLister], {
+    buttonLabel: "Create New",
+    buttonCreateLabel: "Create",
+    buttonUpdateLabel: "Save",
+    deleteConfirmation: "Are you sure?",
+    deleteConfirmPosition: NaN,
 
-	buttonLabel: "Create New"
-	,buttonCreateLabel: "Create"
-	,buttonUpdateLabel: "Save"
-	,deleteConfirmation: "Are you sure?"
-    ,deleteConfirmPosition: NaN
+    canCreate: true,
+    canUpdate: true,
+    canDelete: true,
+    checkEachUpdate: false,
+    checkEachDelete: false,
 
-	,canCreate: true
-	,canUpdate: true
-	,canDelete: true
+    _confirmDeleteDialog: null,
+    _deleteButton: null,
+    _cancelDeleteButton: null,
 
-	,checkEachUpdate: false
-	,checkEachDelete: false
+    postCreate: function () {
+        this.inherited(arguments);
 
-	,_confirmDeleteDialog: null
-	,_deleteButton: null
-	,_cancelDeleteButton: null
+        if (this.canDelete && null !== this.deleteConfirmation) {
+            this._confirmDeleteDialog = new dijit.Dialog({
+                title: 'Delete Confirmation',
+                content: this.deleteConfirmation + '<br/>',
+                onHide: dojo.hitch(this, this._cancelDelete),
+                draggable: this.draggable
+            });
 
-	,postCreate:function(){
-		this.inherited(arguments);
+            this._deleteButton = new dijit.form.Button({
+                label: "Yes",
+                onClick: dojo.hitch(this, this._deleteActive)
+            }).placeAt(this._confirmDeleteDialog.containerNode);
 
-		if(this.canDelete && null != this.deleteConfirmation) {
-			this._confirmDeleteDialog = new dijit.Dialog({
-				title: 'Delete Confirmation',
-				content: this.deleteConfirmation + '<br/>',
-				onHide: dojo.hitch(this, this._cancelDelete),
-				draggable: this.draggable
-			});
-
-			this._deleteButton = new dijit.form.Button({
-				label:"Yes",
-				onClick: dojo.hitch(this, this._deleteActive)
-			}).placeAt(this._confirmDeleteDialog.containerNode);
-
-			dojo.create('span',{innerHTML:'&nbsp;&nbsp;'}, this._confirmDeleteDialog.containerNode);
-			this._cancelDeleteButton = new dijit.form.Button({
-				label: "Cancel",
-				baseClass: "gray dijitButton",
-				onClick: dojo.hitch(this, function(){this._confirmDeleteDialog.hide();})
-			}).placeAt(this._confirmDeleteDialog.containerNode);
-		}
-	}
-
-	,startup:function(){
-		this.inherited(arguments);
-
-		if(!this.canCreate){
-			dojo.addClass(this.buttonNode.domNode, "dijitHidden");
-		}
-	}
-
-	,_onItemClick: function(widget, traversable, evt) {
-		var link = traversable.closest('a')[0];
-		if(null != link) {
-			var type = dojo.attr(link, 'data-dojo-attach-point');
-			if(type == 'deleteAnchor'){
-				dojo.stopEvent(evt);
-				if(this._confirmDeleteDialog) {
-					this._showDeleteConfirmation();
-				} else {
-					this._deleteActive();
-				}
-			} else if(type == 'updateAnchor'){
-				dojo.stopEvent(evt);
-				this._prepFormForUpdate();
-			} else {
-				this._activateItem(widget,traversable);
-			}
-		} else {
-			this._activateItem(widget,traversable);
-		}
-	}
-
-    ,_showDeleteConfirmation: function(){
-        this._confirmDeleteDialog.show();
-        if(!isNaN(this.deleteConfirmPosition)){
-            dojo.style(this._confirmDeleteDialog.domNode,'top',this.deleteConfirmPosition + 'px');
+            dojo.create('span', {
+                innerHTML: '&nbsp;&nbsp;'
+            }, this._confirmDeleteDialog.containerNode);
+            this._cancelDeleteButton = new dijit.form.Button({
+                label: "Cancel",
+                baseClass: "gray dijitButton",
+                onClick: dojo.hitch(this, function () {
+                    this._confirmDeleteDialog.hide();
+                })
+            }).placeAt(this._confirmDeleteDialog.containerNode);
         }
-    }
+    },
 
-	,_getCurrentData: function(){
-		return this._activeItem.get("value");
-	}
+    startup: function () {
+        this.inherited(arguments);
 
-	,_addRecord: function(data){
+        if (!this.canCreate) {
+            dojo.addClass(this.buttonNode.domNode, "dijitHidden");
+        }
+    },
+
+    _onItemClick: function (widget, traversable, evt) {
+        var link = traversable.closest('a')[0],
+            type;
+
+        this._activateItem(widget, traversable);
+        if (null !== link) {
+            type = dojo.attr(link, 'data-dojo-attach-point');
+            if (type === 'deleteAnchor') {
+                dojo.stopEvent(evt);
+                if (this._confirmDeleteDialog) {
+                    this._showDeleteConfirmation();
+                } else {
+                    this._deleteActive();
+                }
+            } else if (type === 'updateAnchor') {
+                dojo.stopEvent(evt);
+                this._prepFormForUpdate();
+            }
+        }
+    },
+
+    _showDeleteConfirmation: function () {
+        this._confirmDeleteDialog.show();
+        if (!isNaN(this.deleteConfirmPosition)) {
+            dojo.style(this._confirmDeleteDialog.domNode, 'top', this.deleteConfirmPosition + 'px');
+        }
+    },
+
+    _getCurrentData: function () {
+        return this._activeItem.get("value");
+    },
+
+    _addRecord: function (data) {
         this._setPermissions(data);
-		return this.inherited(arguments);
-	}
+        return this.inherited(arguments);
+    },
 
-	,_submitForm: function(){
-		var data = this._getFormData();
-		if(this._currentAction == "create") {
-			this._createNew(data);
-		} else if(this._currentAction == "update") {
-			this._updateActive(data)
-		}
-	}
+    _submitForm: function () {
+        var data = this._getFormData();
+        if (this._currentAction === "create") {
+            this._createNew(data);
+        } else if (this._currentAction === "update") {
+            this._updateActive(data);
+        }
+    },
 
-    ,_getFormData: function(){
-        if(typeof this._form == "object"){
+    _getFormData: function () {
+        if (typeof this._form === "object") {
             return this._form.get("value");
         } else {
             console.error("no form found in this widget");
             return null;
         }
-    }
+    },
 
-	,_onCancel: function(){
-		this._cancel(this._actionButton);
-	}
+    _onCancel: function () {
+        this._cancel(this._actionButton);
+    },
 
-	,_cancelDelete: function(){
-		this._cancel(this._deleteButton);
-	}
+    _cancelDelete: function () {
+        this._cancel(this._deleteButton);
+    },
 
-	,_cancel: function(button){
-		if(null !== this._lastDeferred){
-			this._lastDeferred.cancel();
-		}
+    _cancel: function (button) {
+        if (null !== this._lastDeferred) {
+            this._lastDeferred.cancel();
+        }
 
-		if(null !== this._errorTooltip){
-			this._errorTooltip.close();
-			this._errorTooltip.removeTarget(button);
-			button.set("iconClass","");
-		}
-	}
+        if (null !== this._errorTooltip) {
+            this._errorTooltip.close();
+            this._errorTooltip.removeTarget(button);
+            button.set("iconClass", "");
+        }
+    },
 
-	,_onButtonNodeClick: function(){
-		this.inherited(arguments);
-		this._prepFormForCreation();
-	}
+    _onButtonNodeClick: function () {
+        this.inherited(arguments);
+        this._prepFormForCreation();
+    },
 
-	,_prepFormForCreation: function(){
-		this._currentAction = "create";
-		this._form.reset();
-		this._actionButton.set("label",this.buttonCreateLabel);
-		var items = this._form.getDescendants();
-		var i;
-		for(i=0; i<items.length; i++){
-			var item = items[i];
-			var node = item.domNode;
-			var placeholder = dojo.query(node).siblings('[data-placeholder-for$='+ item.id + ']')[0];
-			if(placeholder != null) {
-				dojo.addClass(placeholder, 'dijitHidden');
-				dojo.removeClass(node, 'dijitHidden');
-				item.set("disabled", false);
-			}
-		}
-	}
+    _prepFormForCreation: function () {
+        this._currentAction = "create";
+        this._form.reset();
+        this._actionButton.set("label", this.buttonCreateLabel);
+        var items = this._form.getDescendants(),
+            item,
+            node,
+            placeholder,
+            i;
+        for (i = 0; i < items.length; ++i) {
+            item = items[i];
+            node = item.domNode;
+            placeholder = dojo.query(node).siblings('[data-placeholder-for$=' + item.id + ']')[0];
+            if (placeholder !== null) {
+                dojo.addClass(placeholder, 'dijitHidden');
+                dojo.removeClass(node, 'dijitHidden');
+                item.set("disabled", false);
+            }
+        }
+    },
 
-	,_createNew: function(data){
-		this._lastDeferred = this.objectStore.add(data);
-		dojo.when(
-			this._lastDeferred,
-			dojo.hitch(this, this._newCreated),
-			dojo.hitch(this, this._requestError)
-		);
-	}
+    _createNew: function (data) {
+        this._lastDeferred = this.objectStore.add(data);
+        dojo.when(
+            this._lastDeferred,
+            dojo.hitch(this, this._newCreated),
+            dojo.hitch(this, this._requestError)
+        );
+    },
 
-    ,_setPermissions: function(data){
-        if(!data.hasOwnProperty('canUpdate')){
+    _setPermissions: function (data) {
+        if (!data.hasOwnProperty('canUpdate')) {
             data.canUpdate = true;
         }
 
-        if(!data.hasOwnProperty('canDelete')){
+        if (!data.hasOwnProperty('canDelete')) {
             data.canDelete = true;
         }
 
-        if(!this.canUpdate) {
+        if (!this.canUpdate) {
             data.canUpdate = false;
         }
 
-        if(!this.canDelete) {
+        if (!this.canDelete) {
             data.canDelete = false;
         }
-    }
+    },
 
-	,_newCreated: function(data){
-		var item = this._addRecord(data, true);
-		this._activateItem(item);
-	}
+    _newCreated: function (data) {
+        var item = this._addRecord(data, true);
+        this._activateItem(item);
+    },
 
-	,_prepFormForUpdate: function(){
-		this._currentAction = "update";
-		var data = this._getCurrentData();
-		var raw = this._activeItem.get("rawProperties");
-		this._form.reset();
-		this._form.set("value",data);
-		this._actionButton.set("label",this.buttonUpdateLabel);
-		var items = this._form.getDescendants();
-		var i;
-		for(i=0; i<items.length; i++){
-			var item = items[i];
-			var node = item.domNode;
-			var placeholder = dojo.query(node).siblings('[data-placeholder-for$='+ item.id + ']')[0];
-			if(placeholder != null) {
-				dojo.removeClass(placeholder, 'dijitHidden');
-				var value = raw[item.id];
-				if(typeof value === 'object'){value = value.label;}
-				placeholder.innerHTML = value;
-				dojo.addClass(node, 'dijitHidden');
-				item.set("disabled", true);
-			}
-		}
-	}
+    _prepFormForUpdate: function () {
+        this._currentAction = "update";
+        var data = this._getCurrentData(),
+            raw = this._activeItem.get("rawProperties"),
+            items,
+            i,
+            item,
+            node,
+            value,
+            placeholder;
 
-	,_updateActive: function(data){
-		this._lastDeferred = this.objectStore.put(data, {id: this._activeItem.getId()});
-		dojo.when(
-			this._lastDeferred,
-			dojo.hitch(this, this._recordUpdated),
-			dojo.hitch(this, this._requestError)
-		);
-	}
+        this._form.reset();
+        this._form.set("value", data);
+        this._actionButton.set("label", this.buttonUpdateLabel);
+        items = this._form.getDescendants();
+        for (i = 0; i < items.length; ++i) {
+            item = items[i];
+            node = item.domNode;
+            placeholder = dojo.query(node).siblings('[data-placeholder-for$=' + item.id + ']')[0];
+            if (placeholder !== null) {
+                dojo.removeClass(placeholder, 'dijitHidden');
+                value = raw[item.id];
+                if (typeof value === 'object') {
+                    value = value.label;
+                }
 
-	,_recordUpdated: function(data){
+                placeholder.innerHTML = value;
+                dojo.addClass(node, 'dijitHidden');
+                item.set("disabled", true);
+            }
+        }
+    },
+
+    _updateActive: function (data) {
+        this._lastDeferred = this.objectStore.put(data, {
+            id: this._activeItem.getId()
+        });
+        dojo.when(
+            this._lastDeferred,
+            dojo.hitch(this, this._recordUpdated),
+            dojo.hitch(this, this._requestError)
+        );
+    },
+
+    _recordUpdated: function (data) {
+        var widget;
         this._setPermissions(data);
         widget = this.dataItems[data[this.idProperty]];
-        widget.set("value",data);
-        if(widget == this._activeItem) {
+        widget.set("value", data);
+        if (widget === this._activeItem) {
             this._activateItem(widget);
         }
-	}
+    },
 
-	,_requestError:function(error){
-		console.error("An error occured:", error);
-		if(error.invalid){
-			this._onInvalid(error.invalid);
-		} else {
-			this.onError(error);
-		}
-	}
+    _requestError: function (error) {
+        console.error("An error occured:", error);
+        if (error.invalid) {
+            this._onInvalid(error.invalid);
+        } else {
+            this.onError(error);
+        }
+    },
 
-	,_deleteActive: function(){
-		if(this._deleteButton.get("disabled")) return;
+    _deleteActive: function () {
+        if (this._deleteButton.get("disabled")) {
+            return;
+        }
 
-		this._deleteButton.set("disabled",true);
-		this._deleteButton.set("iconClass","dijitIconWaiting");
+        this._deleteButton.set("disabled", true);
+        this._deleteButton.set("iconClass", "dijitIconWaiting");
 
-		if(this._errorTooltip != null){
-			this._errorTooltip.close();
-			this._errorTooltip.removeTarget(this._deleteButton.domNode);
-		}
+        if (this._errorTooltip !== null) {
+            this._errorTooltip.close();
+            this._errorTooltip.removeTarget(this._deleteButton.domNode);
+        }
 
-		this._lastDeferred = this.objectStore.remove(this._activeItem.getId());
-		dojo.when(
-			this._lastDeferred,
-			dojo.hitch(this, this._recordDeleted),
-			dojo.hitch(this, this._deleteRequestError)
-		);
-	}
+        this._lastDeferred = this.objectStore.remove(this._activeItem.getId());
+        dojo.when(
+            this._lastDeferred,
+            dojo.hitch(this, this._recordDeleted),
+            dojo.hitch(this, this._deleteRequestError)
+        );
+    },
 
-	,_recordDeleted: function(data){
-		if(this._confirmDeleteDialog != null) {this._confirmDeleteDialog.hide();}
-		this._deleteButton.set("disabled",false);
-		this._deleteButton.set('iconClass',"");
+    _recordDeleted: function (data) {
+        var widget;
+        if (this._confirmDeleteDialog !== null) {
+            this._confirmDeleteDialog.hide();
+        }
+
+        this._deleteButton.set("disabled", false);
+        this._deleteButton.set('iconClass', "");
 
         widget = this.dataItems[data.id];
         delete this.dataItems[data.id];
 
-        if(widget == this._activeItem) {
+        if (widget === this._activeItem) {
             this._activateItem(null);
         }
-		widget.destroy();
-	}
 
-	,_deleteRequestError:function(error){
-		console.error("An error occured:", error);
-		this._deleteButton.set("disabled", false);
-		this._deleteButton.set("iconClass", "dijitIconError");
-		this._initErrorTooltip();
-		this._errorTooltip.addTarget(this._deleteButton.domNode);
-		this._errorTooltip.open(this._deleteButton.domNode);
-	}
+        widget.destroy();
+    },
 
+    _deleteRequestError: function (error) {
+        console.error("An error occured:", error);
+        this._deleteButton.set("disabled", false);
+        this._deleteButton.set("iconClass", "dijitIconError");
+        this._initErrorTooltip();
+        this._errorTooltip.addTarget(this._deleteButton.domNode);
+        this._errorTooltip.open(this._deleteButton.domNode);
+    }
 });
